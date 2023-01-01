06d7a730cda0b577f9764e59075bbd9ca8a47252
DanielRabl <danielrabl1999@gmail.com>
2023-01-01 20:43:17 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@ int main() try {
 	qpl::println("press VK_XBUTTON2 to boost.");
 	qpl::println("double press LCTR  to change speed.");
 
+	qpl::cubic_generator random_range;
+	random_range.set_random_range(tick * 0.5, tick * 1.5);
+	random_range.set_speed(0.5);
+
 	qpl::f64 random_tick = tick * qpl::random(0.5, 1.5);
 	qpl::small_clock clock;
 	qpl::small_clock lcontrol_clock;
@@ -19,6 +23,7 @@ int main() try {
 	bool boost = false;
 
 	bool autoclicker = false;
+	qpl::small_clock frame_time;
 	while (true) {
 		bool lcontrol = qpl::winsys::key_holding(VK_LCONTROL);
 		bool lcontrol_pressed = qpl::winsys::key_pressed(VK_LCONTROL);
@@ -38,6 +43,7 @@ int main() try {
 		}
 		if (lcontrol_once_pressed && lcontrol_pressed && lcontrol_clock.elapsed_f() < 0.5) {
 			tick = get_tickspeed();
+			random_range.set_random_range(tick * 0.5, tick * 1.5);
 		}
 		if (lcontrol_pressed) {
 			lcontrol_clock.reset();
@@ -45,13 +51,15 @@ int main() try {
 		}
 
 		if (autoclicker) {
-			if (clock.has_elapsed_reset(random_tick)) {
-				random_tick = tick * qpl::random(0.5, 1.5);
-				if (boost) random_tick /= 4;
+			random_range.update(frame_time.elapsed_f());
+			auto delta = random_range.get();
 
+			if (boost) delta /= 4;
+			if (clock.has_elapsed_reset(delta)) {
 				qpl::winsys::click_left_mouse();
 			}
 		}
+		frame_time.reset();
 	}
 
 }
