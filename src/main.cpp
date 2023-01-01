b5e6c59ba60ac45eb14bffd717d87ebe13670f10
DanielRabl <danielrabl1999@gmail.com>
2023-01-01 20:21:17 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,14 @@ int main() try {
 	qpl::f64 tick = 1.0 / 30.0;
 
 	qpl::println("press VK_XBUTTON1 to toggle autoclick.");
+	qpl::println("press VK_XBUTTON2 to boost.");
 	qpl::println("double press LCTR  to change speed.");
 
 	qpl::f64 random_tick = tick * qpl::random(0.5, 1.5);
 	qpl::small_clock clock;
 	qpl::small_clock lcontrol_clock;
 	bool lcontrol_once_pressed = false;
+	bool boost = false;
 
 	bool autoclicker = false;
 	while (true) {
@@ -31,6 +33,9 @@ int main() try {
 				qpl::println("autoclicker OFF");
 			}
 		}
+		if (qpl::winsys::key_pressed(VK_XBUTTON2)) {
+			boost = !boost;
+		}
 		if (lcontrol_once_pressed && lcontrol_pressed && lcontrol_clock.elapsed_f() < 0.5) {
 			tick = get_tickspeed();
 		}
@@ -42,6 +47,8 @@ int main() try {
 		if (autoclicker) {
 			if (clock.has_elapsed_reset(random_tick)) {
 				random_tick = tick * qpl::random(0.5, 1.5);
+				if (boost) random_tick /= 4;
+
 				qpl::winsys::click_left_mouse();
 			}
 		}
