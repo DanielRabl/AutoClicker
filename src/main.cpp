c263202f43c859a22ab26c5ab55baed712ca355d
DanielRabl <danielrabl1999@gmail.com>
2023-01-02 15:26:12 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,9 @@ int main() try {
 	random_range.set_random_range(-1.0, 1.0);
 	random_range.set_speed(1.5);
 
+	qpl::animation activate_boost;
+	activate_boost.set_duration(1.0);
+
 	qpl::f64 random_tick = tick * qpl::random(0.5, 1.5);
 	qpl::small_clock clock;
 	qpl::small_clock lcontrol_clock;
@@ -25,15 +28,32 @@ int main() try {
 	bool autoclicker = false;
 	qpl::small_clock frame_time;
 	while (true) {
+		auto frame_t = frame_time.elapsed_f();
+		activate_boost.update(frame_t);
+
 		bool lcontrol = qpl::winsys::key_holding(VK_LCONTROL);
 		bool lcontrol_pressed = qpl::winsys::key_pressed(VK_LCONTROL);
 
+		auto toggle = qpl::winsys::key_pressed(VK_XBUTTON1);
+		if (lcontrol && qpl::winsys::key_pressed(' ')) {
+			toggle = true;
+		}
 
-		if (qpl::winsys::key_pressed(VK_XBUTTON1)) {
+		if (toggle) {
 			autoclicker = !autoclicker;
+			if (!autoclicker) {
+				boost = false;
+				activate_boost.go_backwards();
+			}
 		}
 		if (autoclicker && qpl::winsys::key_pressed(VK_XBUTTON2)) {
 			boost = !boost;
+			if (boost) {
+				activate_boost.go_forwards();
+			}
+			else {
+				activate_boost.go_backwards();
+			}
 		}
 		if (lcontrol_once_pressed && lcontrol_pressed && lcontrol_clock.elapsed_f() < 0.5) {
 			tick = get_tickspeed();
@@ -44,11 +64,11 @@ int main() try {
 		}
 
 		if (autoclicker) {
-			random_range.update(frame_time.elapsed_f());
+			random_range.update(frame_t);
 			auto exp = std::pow(2, random_range.get());
 			auto delta = tick * exp;
 
-			if (boost) delta /= 4;
+			delta *= qpl::linear_interpolation(1.0, 0.25, activate_boost.get_curve_progress());
 			if (clock.has_elapsed_reset(delta)) {
 				qpl::winsys::click_left_mouse();
 			}
